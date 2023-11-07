f2140df38948d076f2b99baf1df22cf59bc8b7a6
Logon Aluno <logonrmlocal@fiap.com.br>
2023-11-07 11:22:14 -0300

    codigo funcionando

--- homestyle.js
+++ homestyle.js
@@ -1,66 +1,90 @@
-const express = require("express");
+//requisitando os modulos
 const mongoose = require("mongoose");
+const express = require("express");
 const bodyParser = require("body-parser");
 
-const app = express;
-
+//configurando o express para o postman e para usar a pagina
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
 const port = 3000;
-mongoose.connect("mongodb://127.0.0.1:27017/homestyle",{  
-  useNewUrlParser: true,  
+
+//configurando o banco de dados
+mongoose.connect("mongodb://127.0.0.1:27017/homestyle", {
+  useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-const UsuarioSchema = new mongoose.Schema({  
-  email: { type: String, required: true },  
-  senha: { type: String}
+//criando a model usuario do meu projeto
+const UsuarioSchema = new mongoose.Schema({
+    email : {type : String, required : true},
+    senha : {type : String}
 });
 
-const produtoDecoracaoSchema = new mongoose.Schema({  
-  id_produtodecoracao : {type: mongoose.ObjetcId, required: true},
-  descricao: {type: String},
-  fornecedor: {type: String},
-  dataFabricacao: {type: Date},
-  quantidadeEstoque: {type: Number}
-});
+const produtoDecoracaoSchema = new mongoose.Schema({
+    id_produtodecoracao : {type : String, required : true},
+    descricao : {type : String},
+    fornecedor : {type : String},
+    datafabricacao : {type : Date},
+    quantidadeestoque : {type : Number}
+})
 
 const Usuario = mongoose.model("Usuario", UsuarioSchema);
-const Produto = mongoose.model("Produto", produtoDecoracaoSchema);
 
-app.post("/cadastroUsuario", async (req, res) =>{
-   const email = req.body.email;
-   const senha = req.body.senha;
-   const usuario = new Usuario({ 
-     email: email,
-     senha: senha
-   })
-try {
-  const newUsuario = await usuario.save();    
-  res.json({ error: null, msg: "Cadastro ok", UsuarioId: newUsuario._id });  
-} catch (error) {}})
+const produtoDecoracao = mongoose.model("ProdutoDecoracao",produtoDecoracaoSchema)
 
-app.post("/cadastroProdutoDecoracao", async (req, res) =>{
-   const id_produtodecoracao = req.body.id_produtodecoracao;
-   const descricao = req.body.descricao;
-   const fornecedor = req.body.fornecedor;
-   const dataFabricacao = req.body.dataFabricacao;
-   const quantidadeEstoque = req.body.quantidadeEstoque;
-   const produto = new Produto({ 
-     id_produtodecoracao: id_produtodecoracao,
-     descricao: descricao,
-     fornecedor: fornecedor,
-     dataFabricacao: dataFabricacao,
-     quantidadeEstoque: quantidadeEstoque,
-   })
-try {
-  const newProduto = await produto.save();    
-  res.json({ error: null, msg: "Cadastro ok", ProdutoId: newProduto._id });  
-} catch (error) {}})
-
-app.get("/", async()=>{
-res.sendFile(__dirname + "/index.html")
+//configuração dos roteamendos
+//cadastrousuario
+app.post("/cadastrousuario", async (req, res) => {
+  const email = req.body.email;
+  const senha = req.body.senha;
+  
+   
+  const usuario = new Usuario({
+    email: email,
+    senha: senha
 });
 
-app.listen(port, ()=>{    
-  console.log(`Servidor rodanda na porta ${port}`)
+  try {
+    const newUsuario = await usuario.save();
+    res.json({ error: null, msg: "Cadastro ok", UsuarioId: newUsuario._id });
+  } catch (error) {}
+
+});
+
+
+//rota de cadastro especifico
+app.post("/cadastroprodutodecoracao", async (req, res) => {
+    
+    
+    const id_produtodecoracao = req.body.id_produtodecoracao;
+    const descricao = req.body.descricao;
+    const fornecedor = req.body.id_fornecedor;
+    const datafabricacao = req.body.datafabricacao;
+    const quantidadeestoque = req.body.quantidadeestoque;
+     
+    const produtodecoracao = new produtoDecoracao({
+      id_produtodecoracao: id_produtodecoracao,
+      descricao: descricao,
+      fornecedor: fornecedor,
+      datafabricacao: datafabricacao,
+      quantidadeestoque: quantidadeestoque
+    });
+  
+    try {
+      const newProdutoDecoracao = await produtodecoracao.save();
+      res.json({ error: null, msg: "Cadastro ok", ProdutocabeloId: newProdutoDecoracao._id });
+    } catch (error) {}
+  
+  });
+
+//rota padrao
+app.get("/", async (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+//tem que ter o comando de listen
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
 });
